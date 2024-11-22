6357a5c886a27a2d0bc6efa739fb1323262d193b
Farhan <mhmmdfarhann@gmail.com>
2024-11-22 14:10:18 +0700

    Latihan 2

---
+++ pages/home.php
@@ -0,0 +1,37 @@
+<div class="container-fluid mb-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-xxl-7">
+                <div class="d-flex justify-content-center mt-5 mt-xxl-0">
+                    <div class="profile bg-gradient-primary-to-secondary">
+                        <img class="profile-img" src="img\Pas_Foto.png" alt="..." />
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-5">
+                <div class="text-center text-xxl-start">
+                    <div class="fs-2 fw-light text-muted ">Active Learner</div>
+                    <h1 class="display-2 fw-bolder mb-4"><span class="d-inline">Muhammad Farhan Saputra</span></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Tentang Saya-->
+    <section class="bg-light py-5">
+        <div class="container px-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-xxl-8">
+                    <div class="text-center my-5">
+                        <h2 class="display-5 fw-bolder"><span>Tentang Saya</span></h2>
+                        <p class="lead fw-light mb-4">Nama Saya Muhammad Farhan Saputra</p>
+                        <p class="text-muted">Mahasiswa aktif Fakultas Teknologi Informasi di bidang Informatika. Saya memiliki minat yang besar dalam ilmu komputer dan informatika dan telah aktif berorganisasi di masa perkuliahan</p>
+                        <p class="text-muted"><i class="fa-solid fa-phone"></i> 081272855658 | <i class="fa-solid fa-envelope"></i> mhmmdfarhann05@gmail.com</p>
+                        <div class="d-flex justify-content-center fs-2 gap-4">
+                            <a class="" href="https://www.instagram.com/farhnsaptr"><i class="fa-brands fa-instagram" style="color: #0d6efd;"></i></a>
+                            <a class="" href="https://github.com/katanagore"><i class="fa-brands fa-github" style="color: #0d6efd;"></i></a>
+                            <a class="" href="https://www.linkedin.com/in/farhnsaptr/"><i class="fa-brands fa-linkedin" style="color: #0d6efd;"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
---
+++ pages/resume.php
@@ -0,0 +1,156 @@
+<div class="container px-5 my-5">
+        <div class="text-center mb-5">
+            <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Curriculum Vitae</span></h1>
+        </div>
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-11 col-xl-9 col-xxl-8">
+                <!-- Pendidikan Section-->
+                <section>
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h2 class="text-primary fw-bolder mb-0">Pendidikan</h2>
+                    </div>
+                    <!-- Pendidikan Card 1-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <div class="row align-items-center gx-5">
+                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                    <div class="bg-light p-4 rounded-4">
+                                        <div class="text-primary fw-bolder mb-2">Jun 2017 – Mei 2020</div>
+                                        <div class="small fw-bolder">MTsN 2 Bungo</div>
+                                        <div class="small text-muted">Rata-rata : 90</div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8"><div>Sekolah Menengah Pertama di MTsN 2 Bungo, Jambi. Menumbuhkan minat di Informatika dan Komputer</div></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Pendidikan Card 2-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <div class="row align-items-center gx-5">
+                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                    <div class="bg-light p-4 rounded-4">
+                                        <div class="text-primary fw-bolder mb-2">April 2020 - Jun 2023 </div>
+                                        <div class="small fw-bolder">MAN Insan Cendekia Jambi</div>
+                                        <div class="small text-muted">Rata-rata : 92,90</div>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-lg-8"><div>Sekolah Menengah Akhir di MAN Insan Cendekia Jambi. Mengembangkan minat informatika dan komputer melalui olimpiade dan lomba</div></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Pendidikan Card 3-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <div class="row align-items-center gx-5">
+                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                    <div class="bg-light p-4 rounded-4">
+                                        <div class="text-primary fw-bolder mb-2">Jun 2023 – Sekarang</div>
+                                        <div class="small fw-bolder">Universitas Andalas</div>
+                                        <div class="small text-muted">IPK : 3.68</div>
+                                        
+                                    </div>
+                                </div>
+                                <div class="col-lg-8"><div>Menempuh pendidikan lanjutan di Universitas Andalas. Sedang menekuni di Bidang Data Science, Backend Developing, dan Software Engineering</div></div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- Pengalaman Section-->
+                <section>
+                    <h2 class="text-primary fw-bolder mb-4">Pengalaman</h2>
+                    <!-- Pengalaman Card 1-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <div class="row align-items-center gx-5">
+                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                    <div class="bg-light p-4 rounded-4">
+                                        <div class="text-primary fw-bolder mb-2">11-12 Desember 2021</div>
+                                        <div class="mb-2">
+                                            <div class="small fw-bolder">PJ Konsumsi</div>
+                                            <div class="small text-muted">Classic 2022</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-8"><div>Mendistribusikan konsumsi kepada panitia-panitia acara Classic MAN Insan Cendekia Jambi, Mengatur pembelanjaan terhadap konsumsi panitia agar tidak melebihi anggaran pembelanjaan, Mendistribusikan konsumsi kepada peserta lomba Classic MAN Insan Cendekia Jambi</div></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Pengalaman Card 2-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <div class="row align-items-center gx-5">
+                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                    <div class="bg-light p-4 rounded-4">
+                                        <div class="text-primary fw-bolder mb-2">Agustus 2024</div>
+                                        <div class="mb-2">
+                                            <div class="small fw-bolder">Staff Konsumsi</div>
+                                            <div class="small text-muted">BAKTI Universitas Andalas 2024</div>
+                                        </div>  
+                                    </div>
+                                </div>
+                                <div class="col-lg-8"><div>Bertanggung jawab untuk mendistribusikan konsumsi kepada kelompok mahasiswa baru Universitas Andalas, Mendistribusikan konsumsi untuk panitia BAKTI Universitas Andalas 2024</div></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Pengalaman card 3-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <div class="row align-items-center gx-5">
+                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                    <div class="bg-light p-4 rounded-4">
+                                        <div class="text-primary fw-bolder mb-2">September 2024</div>
+                                        <div class="mb-2">
+                                            <div class="small fw-bolder">Staff Sponsorship</div>
+                                            <div class="small text-muted">LKMMTD-FTI Universitas Andalas 2024</div>
+                                        </div>  
+                                    </div>
+                                </div>
+                                <div class="col-lg-8"><div>Bertanggung jawab untuk menjalin kerjasama antar perusahaan via email</div></div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- Divider-->
+                <div class="pb-5"></div>
+                <!-- Skills Section-->
+                <section>
+                    <!-- Skillset Card-->
+                    <div class="card shadow border-0 rounded-4 mb-5">
+                        <div class="card-body p-5">
+                            <!-- Professional skills list-->
+                            <div class="mb-5">
+                                <div class="d-flex align-items-center mb-4">
+                                    <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-tools"></i></div>
+                                    <h3 class="fw-bolder mb-0"><span class="text-primary d-inline">Professional Skills</span></h3>
+                                </div>
+                                <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-regular fa-lightbulb me-1" style="color: #0d6efd;"></i>Logical Thinking</div></div>
+                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-solid fa-check me-1" style="color: #0d6efd;"></i> Problem Solving</div></div>
+                                    <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-solid fa-user-tie me-1" style="color: #0d6efd;"></i>Leadership</div></div>
+                                </div>
+                                <div class="row row-cols-1 row-cols-md-3">
+                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-solid fa-user-secret me-1" style="color: #0d6efd;"></i>Network Security</div></div>
+                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-solid fa-database me-1" style="color: #0d6efd;"></i>Backend Developer</div></div>
+                                    <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-solid fa-server me-1" style="color: #0d6efd;"></i>Software Engineering</div></div>
+                                </div>
+                            </div>
+                            <!-- Languages list-->
+                            <div class="mb-0">
+                                <div class="d-flex align-items-center mb-4">
+                                    <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
+                                    <h3 class="fw-bolder mb-0"><span class="text-primary d-inline">Bahasa Pemrograman</span></h3>
+                                </div>
+                                <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-brands fa-html5 me-1" style="color: #0d6efd;"></i>HTML</div></div>
+                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-brands fa-css3-alt me-1" style="color: #0d6efd;"></i>CSS</div></div>
+                                    <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100"><i class="fa-brands fa-java me-1" style="color: #0d6efd;"></i>Java</div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
