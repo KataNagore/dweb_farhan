852b306168f40df4f72ad0774ce77e7576b1d897
Farhan <mhmmdfarhann@gmail.com>
2024-11-29 15:12:59 +0700

    Update lagi

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light py-3">
         <div class="container px-5">
-            <a class="navbar-brand" href="index.php"><span class="fw-bolder text-primary">Farhan</span></a>
+            <a class="navbar-brand" href="index.html"><span class="fw-bolder text-primary">Farhan</span></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=home">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=resume">About</a></li>
-                    <li class="nav-item"><a class="nav-link" href="index.php?page=blog">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.html?page=home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.html?page=resume">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.html?page=blog">Blog</a></li>
                 </ul>
             </div>
         </div>
